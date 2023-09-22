e86fc7a16bcee725b3f196d3deeb31985d46d4c7
mehranhydary <mehranhydary@users.noreply.github.com>
2023-09-22 02:03:07 -0400

    Added base for TakeProfitsHook

--- src/TakeProfitsHook.sol
+++ src/TakeProfitsHook.sol
@@ -2,8 +2,28 @@
 pragma solidity =0.8.19;
 
 import {BaseHook} from "periphery-next/BaseHook.sol";
-import {ERC1155} from "openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {ERC1155} from "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
 
-import {BaseHook} from 
+contract TrakeProfitsHook is BaseHook, ERC1155 {
+    /// @dev Initialize Basehook and ERC1155 parent contracts in the constructor
+    constructor(
+        IPoolManager _poolManager,
+        string memory _url
+    ) BaseHook(_poolManager) ERC1155(_url) {}
 
-contract TrakeProfitsHook is BaseHook, ERC1155 {}
+    function getHooksCall() public pure override returns (Hooks.Calls memory) {
+        return
+            Hooks.Calls({
+                beforeInitialize: false,
+                afterInitialize: true,
+                beforeModifyPosition: false,
+                afterModifyPosition: false,
+                beforeSwap: false,
+                afterSwap: true,
+                beforeDonate: false,
+                afterDonate: false
+            });
+    }
+}
