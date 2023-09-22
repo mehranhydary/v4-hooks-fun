927016f129bfb43301eac56980ed934b2fb2f774
mehranhydary <mehranhydary@users.noreply.github.com>
2023-09-22 02:00:46 -0400

    Adding oz

---
+++ src/TakeProfitsHook.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity =0.8.19;
+
+import {BaseHook} from "periphery-next/BaseHook.sol";
+import {ERC1155} from "openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+import {BaseHook} from 
+
+contract TrakeProfitsHook is BaseHook, ERC1155 {}
